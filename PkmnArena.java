4226a4b92b6d80c7717f40cd7e5d7fae2b38ae5a
Dumfing <dumpling.d.m@gmail.com>
2016-12-16 22:56:57 -0500

    Done

--- PkmnArena.java
+++ PkmnArena.java
@@ -24,7 +24,7 @@ public class PkmnArena{
 	public static Scanner kb = new Scanner(System.in);
 	public static final Random rand = new Random();
 	public static boolean[] options = {false,false,true}; // Pokemon Details, Attack Details, Health and energy results
-	public static String[] optionNames = {"Pokemon Stats", "Attack Details", "Attack Results"};
+	public static String[] optionNames = {"Pokemon Stats", "Attack Details", "Attack Results"}; // String version for displaying to the user
 	public static final int POKEMON_DETAILS = 0;
 	public static final int ATTACK_DETAILS = 1;
 	public static final int RESULT_DETAILS = 2;
@@ -42,88 +42,85 @@ public class PkmnArena{
 	}
 	
 	
-	public static void pickActive(){
-		userParty = Party.pickParty(pokedex);
-		computerParty = Party.computerParty(pokedex,userParty);
-		computerParty.setOwner(botName);
+	public static void pickActive(){ // Let the user pick the pokemon they're going to use
+		userParty = Party.pickParty(pokedex); // let the user pick
+		computerParty = Party.computerParty(pokedex,userParty); // let the computer pick
+		computerParty.setOwner(botName); // the computerParty method defaults to "" as the owner of the party, change the name to the bot name
 		System.out.println("Pick a starting pokemon.");
-		userParty.pickActive(false);
+		userParty.pickActive(false); // Ask the user to pick an active pokemon (false means they can't use the back option)
 	}
 	
 	
-	public static void changeOptions(){
+	public static void changeOptions(){ // Allow the user to toggle the information they want to see from the battle
 		int uIn = -1;
-		while(uIn != 0){
-			System.out.println("0. Back\nToggle...\n1. Pokemon Details "+(options[POKEMON_DETAILS]?"[ON]":"[OFF]")+"\n2. Attack Details"+(options[ATTACK_DETAILS]?"[ON]":"[OFF]")+"\n3. Attack Result Details"+(options[RESULT_DETAILS]?"[ON]":"[OFF]"));
-			uIn = Integer.parseInt(kb.nextLine());
-			if(uIn>0 && uIn <=3){
-					options[uIn-1] = !options[uIn-1];
-					System.out.printf("%s has been turned %s\n",optionNames[uIn-1],options[uIn-1]?"ON":"OFF");
+		while(uIn != 0){ // while the user input isn't back (will also be broken later)
+			System.out.println("0. Back\nToggle...\n1. Pokemon Details "+(options[POKEMON_DETAILS]?"[ON]":"[OFF]")+"\n2. Attack Details"+(options[ATTACK_DETAILS]?"[ON]":"[OFF]")+"\n3. Attack Result Details"+(options[RESULT_DETAILS]?"[ON]":"[OFF]")); // print the state of the options
+			uIn = Integer.parseInt(kb.nextLine()); //get the user's input
+			if(uIn>0 && uIn <=options.length){ // if the user's input maps to an option
+					options[uIn-1] = !options[uIn-1]; //subtract 1 from the input because options are listed 1 to x+1 while the list is 0 to x
+					System.out.printf("%s has been turned %s\n",optionNames[uIn-1],options[uIn-1]?"ON":"OFF"); // tell the user what the setting has changed to
 			}
 		}
 	}
 	
 	
-	public static void battle(){
-		if(rand.nextBoolean()){
-			PkmnTools.computerTurn(userParty,computerParty);
+	public static void battle(){ // used for organizing the user's and computer's turns
+		if(rand.nextBoolean()){ // 50/50 chance of who goes first
+			PkmnTools.computerTurn(userParty,computerParty); // if the computer goes first (normal flow involves the user going first)
 		}
 		while(true){
-			pickNextAction();
-			PkmnTools.computerTurn(userParty,computerParty);
-			if(computerParty.numAlive()==0){
-				System.out.println("You are Trainer Supreme!");
+			pickNextAction(); // prompt the user to pick the next action
+			PkmnTools.computerTurn(userParty,computerParty); // let the computer choose its action
+			if(computerParty.numAlive()==0){ // if the computer has no more living pokemon
+				System.out.println("You are Trainer Supreme!"); // crown the user as the winner
 				break;
 			}
-			else if(userParty.numAlive() == 0){
-				System.out.println("You have no available pokemon!\nYou lose...");
+			else if(userParty.numAlive() == 0){//Tell the user they lost
+				System.out.println("You have no available pokemon!\nYou lose..."); // would be much more creepy if it said "I win"
 				break;
 			}
 		}
 	}
 	
-	public static int pickAttack(Pokemon attacking, Pokemon defending){
-		int nextPhase = SELECTING_ACTION;
-		Integer[] currentAttacks = attacking.availableAttacks();
+	public static boolean pickAttack(Pokemon attacking, Pokemon defending){ //Prompt the user to pick the attack they wish to use, then attacks the computer's pokemon with it
+		Integer[] currentAttacks = attacking.availableAttacks(); // get the attacks the attacking pokemon has enough energy for
 		while(true){
 			System.out.println("Pick an attack:\n 0. Back");
-			if(options[ATTACK_DETAILS]){
-				System.out.println(" s. Simple");
-				for(int i = 0; i<currentAttacks.length;i++){
+			System.out.println(options[ATTACK_DETAILS]?" s. Simple":" d. Details");
+			if(options[ATTACK_DETAILS]){ // if the user wants the detailed view
+				for(int i = 0; i<currentAttacks.length;i++){ // print out the detailed version of the attacks
 					System.out.printf("%2d. %s\n",i+1,attacking.getAttack(currentAttacks[i]).toString());
 				}
 			}
 			else{
-				System.out.println(" d. Details");
-				for(int i = 0;i<currentAttacks.length;i++){
+				for(int i = 0;i<currentAttacks.length;i++){ //print out the names of the attacks
 					System.out.printf("%2d. %s\n",i+1,attacking.getAttack(currentAttacks[i]).getName());
 				}
 			}
-			String uIn = kb.nextLine();
-			if(uIn.equals("d")){
-				options[ATTACK_DETAILS] = true;
+			String uIn = kb.nextLine(); // the user's input is handled as a string first so they can pick more or less details
+			if(uIn.equals("d")){ // if they want more details
+				options[ATTACK_DETAILS] = true; // change setting
 			}
 			else if(uIn.equals("s")){
 				options[ATTACK_DETAILS] = false;
 			}
 			else{
-				int attackNumber = Integer.parseInt(uIn);
-				if(attackNumber>0 && attackNumber < currentAttacks.length+1){
-					System.out.printf("Your %s used %s!\n",attacking.getName(),attacking.getAttack(currentAttacks[attackNumber-1]).getName());
-					attacking.attack(defending,attacking.getAttack(currentAttacks[attackNumber-1]));
-					if(options[RESULT_DETAILS]){	
+				int attackNumber = Integer.parseInt(uIn); // If they aren't trying to change the setting, handle the input as an Integer
+				if(attackNumber>0 && attackNumber < currentAttacks.length+1){ //if the chosen attack is a valid choice
+					System.out.printf("Your %s used %s!\n",attacking.getName(),attacking.getAttack(currentAttacks[attackNumber-1]).getName()); // prompt the user that their pokemon attacked
+					attacking.attack(defending,attacking.getAttack(currentAttacks[attackNumber-1])); // Attack the computer's pokemon
+					if(options[RESULT_DETAILS]){	//if the user wants more details then print more details
 						System.out.printf("Your %s now has %d energy\n",attacking.getName(),attacking.getEnergy());
 						System.out.printf("%s's %s now has %d health\n",botName,defending.getName(),defending.getHealth());
 					}
-					nextPhase = COMPUTER_TURN;
-					break;
+					return true; // the method returns true if the user ended up attacking
 				}
-				else if(attackNumber == 0){
+				else if(attackNumber == 0){ // if the user chose back
 					break;
 				}
 			}
 		}
-	return nextPhase;
+	return false; // the method returns false if the user went back
 	}
 	
 	
@@ -131,43 +128,43 @@ public class PkmnArena{
 	
 	public static void pickNextAction(){
 		boolean pickingAction = true;
-		System.out.println("---------- Your Turn! ----------");
-		if(options[POKEMON_DETAILS){
-			System.out.printf("%s %s Energy: %d/50\n",userParty.currentPokemon().getName(),PkmnTools.makeBar(userParty.currentPokemon().getHealth(),userParty.currentPokemon().getMaxHealth()),userParty.currentPokemon().getEnergy());
+		System.out.println("---------- Your Turn! ----------"); // State that it's the user's turn
+		if(options[POKEMON_DETAILS]){ //if the user wants more details about their pokemon
+			System.out.printf("%s %s Energy: %d/50\n",userParty.currentPokemon().getName(),PkmnTools.makeBar(userParty.currentPokemon().getHealth(),userParty.currentPokemon().getMaxHealth()),userParty.currentPokemon().getEnergy()); //print the health bar and energy
 		}
-		if(userParty.currentPokemon().getStun()){
+		if(userParty.currentPokemon().getStun()){ // if the user's pokemon has been stunned
 			System.out.printf("%s is stunned! Your turn has been skipped\n",userParty.currentPokemon().getName());
-			userParty.currentPokemon().setStun(false);
-			//phase = COMPUTER_TURN;
+			userParty.currentPokemon().setStun(false); // unstun the pokemon
 		}
-		else{
+		else{ // if the user's pokemon hasn't been stunned
 			while(pickingAction){
 				System.out.println("Pick an action:\n1. Attack\n2. Retreat\n3. Pass\n4. Options");
-				uIn = Integer.parseInt(kb.nextLine());
-				switch(uIn+1){
-					case PICKING_ATTACK:
-						if(uIn == 1 && userParty.currentPokemon().availableAttacks().length == 0){
-							System.out.println("Not enough energy for any attacks!"+(options[POKEMON_DETAILS]?String.format("(%s has %d energy)",userParty.currentPokemon().getName(),userParty.currentPokemon().getEnergy()):""));
+				uIn = Integer.parseInt(kb.nextLine()); //get user input
+				switch(uIn+1){ // switch based on input
+					case PICKING_ATTACK: // if the user chooses to attack
+						if(userParty.currentPokemon().availableAttacks().length == 0){ // if the pokemon has no usable attacks
+							System.out.println("Not enough energy for any attacks!"+(options[POKEMON_DETAILS]?String.format("(%s has %d energy)",userParty.currentPokemon().getName(),userParty.currentPokemon().getEnergy()):"")); // if the user wants more detail then it will tell them how much energy the pokemon has
 						}
-						else if(pickAttack(userParty.currentPokemon(),computerParty.currentPokemon()) == COMPUTER_TURN){
-							pickingAction = false;
+						else {
+							pickingAction = !pickAttack(userParty.currentPokemon(),computerParty.currentPokemon()); // pickAttack returns true or false depending on whether the user attacked or not, it will become the bot's turn when pickingAction becomes false
 						}
 					break;
-					case RETREAT:
-						System.out.println("Pick a replacement pokemon.");
-						pickingAction = !userParty.pickActive();
+					case RETREAT: // if the user chooses to retreat
+						System.out.println("Pick a replacement pokemon."); // prompt the user
+						pickingAction = !userParty.pickActive(); // let them pick a new pokemon
 						
 					break;
 					case PASS:
-						pickingAction = false;
+						pickingAction = false; //break the loop
 					break;
 					case OPTIONS:
-						changeOptions();
+						changeOptions(); // let the user change options
 					break;
 					default:
 					break;	
 				}
 			}
+			userParty.restAll(); // rests all the user's pokemon at the end of their turn
 		}
 	}
 	
--- PkmnTools.java
+++ PkmnTools.java
@@ -1,6 +1,6 @@
 //PkmnTools.java
 //Aaron Li
-//Currently only holds a method for reading the name file and picking a random name for the bot
+//Deals with the bot's moves and random naming as well as visualising health bars
 import java.util.Scanner;
 import java.util.ArrayList;
 
@@ -22,71 +22,64 @@ public class PkmnTools{
 		}
 		return possibleNames.get(PkmnArena.rand.nextInt(possibleNames.size())); // pick random index in the arraylist
 	}
-	public static void computerMove(Party computerParty, Party userParty){
-		if(computerParty.currentPokemon().getHealth() >0){
-			Integer[] possibleAttacks = computerParty.currentPokemon().availableAttacks();
-			if(computerParty.currentPokemon().getStun()){
+	public static void computerMove(Party computerParty, Party userParty){ // What happens when it's the Computer's turn to attack
+		if(computerParty.currentPokemon().getHealth() >0){ // If the computer's Pokemon is still alive
+			Integer[] possibleAttacks = computerParty.currentPokemon().availableAttacks(); // get the attacks that the pokemon has enough energy to use
+			if(computerParty.currentPokemon().getStun()){ // will skip the turn if the pokemon is stunned
 				System.out.printf("%s is stunned! The %s's turn has been skipped.\n",computerParty.currentPokemon().getName(),computerParty.getOwner());
 				computerParty.currentPokemon().setStun(false);
 			}
-			else if(possibleAttacks.length > 0){
+			else if(possibleAttacks.length > 0){ //if there are usable attacks for the pokemon to use
 				Pokemon attackingPokemon = userParty.currentPokemon();
-				Attack plannedAttack = computerParty.currentPokemon().getAttack(possibleAttacks[PkmnArena.rand.nextInt(possibleAttacks.length)]);
-				System.out.printf("%s's %s used %s!\n",computerParty.getOwner(),computerParty.currentPokemon().getName(),plannedAttack.getName());
-				computerParty.currentPokemon().attack(attackingPokemon,plannedAttack);
-				if(PkmnArena.options[PkmnArena.RESULT_DETAILS]){
+				Attack plannedAttack = computerParty.currentPokemon().getAttack(possibleAttacks[PkmnArena.rand.nextInt(possibleAttacks.length)]); // get a random attack from the list of possible attacks
+				System.out.printf("%s's %s used %s!\n",computerParty.getOwner(),computerParty.currentPokemon().getName(),plannedAttack.getName()); // print out what the computer chose
+				computerParty.currentPokemon().attack(attackingPokemon,plannedAttack); // attack the user's pokemon with the seslected attack
+				if(PkmnArena.options[PkmnArena.RESULT_DETAILS]){ // if the user wants more details about the attack
 					System.out.printf("%s's %s now has %d energy\n",computerParty.getOwner(),computerParty.currentPokemon().getName(),computerParty.currentPokemon().getEnergy());
 					System.out.printf("Your %s now has %d health\n",userParty.currentPokemon().getName(),userParty.currentPokemon().getHealth());
 				}
 			}
 			else{
-				System.out.println(String.format("%s skipped their turn",computerParty.getOwner()));
+				System.out.println(String.format("%s skipped their turn",computerParty.getOwner())); // What happens when the bot doesn't have enough energy to attack
 			}
 		}
-		else if(computerParty.currentPokemon().getHealth() == 0){
-			userParty.healAll();
-			String oldPokemonName = computerParty.currentPokemon().getName();
+		else if(computerParty.currentPokemon().getHealth() == 0){ // if the computer's bot has fainted
+			userParty.healAll(); // heal all the user's pokemon
+			String oldPokemonName = computerParty.currentPokemon().getName(); // the name of the pokemon that fainted
 			computerParty.setActive(computerParty.getActiveIndex()+1);
-			System.out.printf("%s has fainted, next choice: %s!\n",oldPokemonName,computerParty.currentPokemon().getName());//lisa recommended this
-		}
-		else{
-			
+			System.out.printf("%s has fainted, next choice: %s!\n",oldPokemonName,computerParty.currentPokemon().getName());//print out the bot's next active pokemon
 		}
 	}
-	public static void computerTurn(Party uParty, Party cParty){
+	public static void computerTurn(Party uParty, Party cParty){ // deals with whether the bot has been defeated or not
 		//Computer
-		uParty.restAll();
-		if(uParty.numAlive() == 0){
-			// Thing that happens when the user has no pokemon anymore
+		
+		if(cParty.numAlive()>0){ // if the computer's party still has pokemon
+			System.out.println(String.format("---------- %s's Turn! ----------",cParty.getOwner())); // Start bot's turn
+			computerMove(cParty,uParty); // let the bot pick an attack to use
+			if(uParty.currentPokemon().getHealth()<=0){ // If the user's pokemon has fainted after the bot has attacked
+				System.out.printf("%s has fainted!\n",uParty.currentPokemon().getName());
+				if(uParty.numAlive()>0){ // If the user still has pokemon alive
+					System.out.println("Pick a new pokemon");
+					uParty.pickActive(false); // let the user pick a new starting pokemon
+				}
+			}
 		}
-		else if(cParty.numAlive()>0){
-			System.out.println(String.format("---------- %s's Turn! ----------",cParty.getOwner()));
-			computerMove(cParty,uParty);
-		}
-			//phase = SELECTING_ACTION;
-		else{
-			System.out.printf("%s has fainted!\n",cParty.currentPokemon().getName());
+		else{ // if the computerParty has no living pokemon
+			System.out.printf("%s has fainted!\n",cParty.currentPokemon().getName()); // When the bot's pokemon have all fainted
 			System.out.printf("%s has no more available pokemon!\n",cParty.getOwner());
 		}
-		if(uParty.currentPokemon().getHealth()<=0){
-			System.out.printf("%s has fainted!\n",uParty.currentPokemon().getName());
-			if(cParty.numAlive()>0){
-					System.out.println("Pick a new pokemon");
-					uParty.pickActive(false);
-			}
-		}
-		cParty.restAll();
+		cParty.restAll(); // recharge all the pokemon on the computer's team
 	}
-	public static String makeBar(int currentHealth, int maxHealth){
-		Double remainingHealth = new Double(((float)currentHealth/(float)maxHealth)*10.0);
-		int remainingHealthInt = remainingHealth.intValue();
-		int leftoverHealth = 10-remainingHealthInt;
-		return multiplyLetter(remainingHealthInt,"|")+multiplyLetter(leftoverHealth,":");
+	public static String makeBar(int currentHealth, int maxHealth){ // Make a health bar for printing a visual representation of the pokemon's health
+		Double remainingHealth = new Double(((float)currentHealth/(float)maxHealth)*10.0); // divide with floats first because dividing with integers will lose the decimal places
+		int remainingHealthInt = remainingHealth.intValue(); // convert from Double to int
+		int leftoverHealth = 10-remainingHealthInt; // find the remaining units in the health bar
+		return multiplyLetter(remainingHealthInt,"|")+multiplyLetter(leftoverHealth,":"); // make the health bar with | and :
 	}
-	public static String multiplyLetter(int times, String letter){
+	public static String multiplyLetter(int times, String letter){ // recursively add multiple letters together
 		return multiplyLetter(times,letter,"");
 	}
-	public static String multiplyLetter(int times, String letter, String made){
-		return times == 0?made:multiplyLetter(times-1,letter,made+letter);
+	public static String multiplyLetter(int times, String letter, String made){ 
+		return times == 0?made:multiplyLetter(times-1,letter,made+letter);//call multiplyLetter while adding the letter to the end of made then reducing the number of times it should be added
 	}
 }
